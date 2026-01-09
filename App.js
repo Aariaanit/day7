554b24e289ca23b8a00a7c8cbfc34b67a0b3a354
Aariaanit <arianittershnjaku@gmail.com>
2026-01-09 14:44:25 +0100

    0106

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
-import * as React from 'react';
-import { Button, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import * as React from "react";
+import { Button, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+//import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
-
-import HomeScreen from './src/screens/HomeScreen';
-import AboutScreen from './src/screens/AboutScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-
+import HomeScreen from "./src/screens/HomeScreen";
+import AboutScreen from "./src/screens/AboutScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 //const Tab = createBottomTabNavigator();
 //const Tab = createMaterialBottomTabNavigator();
@@ -20,47 +18,38 @@ const Tab = createMaterialTopTabNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator 
+      <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
             if (route.name === "Home") {
               iconName = "home";
               size = focused ? 25 : 20;
-              color = focused ? '#222' : '#999'
-            }
-            else if(route.name === "About"){
+              color = focused ? "#222" : "#999";
+            } else if (route.name === "About") {
               iconName = "person";
               size = focused ? 25 : 20;
-              color = focused ? '#222' : '#999'
-            }
-            else if(route.name === "Settings"){
+              color = focused ? "#222" : "#999";
+            } else if (route.name === "Settings") {
               iconName = "settings";
               size = focused ? 25 : 20;
-              color = focused ? '#222' : '#999'
+              color = focused ? "#222" : "#999";
             }
-            return(
-              <Ionicons 
-                name={iconName}
-                size={size}
-                color={color}
-              />
-            )
-          }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
         })}
-        tabBarOptions={{ 
-          activeTintColor: '#000',
-          inactiveTintColor: '#f00',
-          activeBackgroundColor: '#999',
-          inactiveBackgroundColor: '#fff',
+        tabBarOptions={{
+          activeTintColor: "#000",
+          inactiveTintColor: "#f00",
+          activeBackgroundColor: "#999",
+          inactiveBackgroundColor: "#fff",
           showLabel: true,
-          labelStyle: {fontSize: 14},
-          showIcon: true
-          
+          labelStyle: { fontSize: 14 },
+          showIcon: true,
         }}
-        activeColor='#00f'
-        inactiveColor='#fff'
-        barStyle={{backgroundColor:'#ff9999'}}
+        activeColor="#00f"
+        inactiveColor="#fff"
+        barStyle={{ backgroundColor: "#ff9999" }}
       >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="About" component={AboutScreen} />
