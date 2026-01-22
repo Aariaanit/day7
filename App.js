de7d40ab551a8a1e51348a74f5312dabe74e0d78
Aariaanit <arianittershnjaku@gmail.com>
2026-01-22 19:59:38 +0100

    Stack

--- App.js
+++ App.js
@@ -1,55 +1,44 @@
 import * as React from "react";
-import { Button, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-//import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-//import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeScreen from "./src/screens/HomeScreen";
 import AboutScreen from "./src/screens/AboutScreen";
 import SettingsScreen from "./src/screens/SettingsScreen";
-import Ionicons from "react-native-vector-icons/Ionicons";
 
-//const Tab = createBottomTabNavigator();
-//const Tab = createMaterialBottomTabNavigator();
-const Tab = createMaterialTopTabNavigator();
+const Tab = createBottomTabNavigator();
 
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: "#fff",
+          },
+          tabBarIcon: ({ focused }) => {
             let iconName;
+
             if (route.name === "Home") {
-              iconName = "home";
-              size = focused ? 25 : 20;
-              color = focused ? "#222" : "#999";
+              iconName = "home-sharp";
             } else if (route.name === "About") {
               iconName = "person";
-              size = focused ? 25 : 20;
-              color = focused ? "#222" : "#999";
             } else if (route.name === "Settings") {
               iconName = "settings";
-              size = focused ? 25 : 20;
-              color = focused ? "#222" : "#999";
             }
-            return <Ionicons name={iconName} size={size} color={color} />;
+
+            return (
+              <Ionicons
+                name={iconName}
+                size={focused ? 26 : 22}
+                color={focused ? "#222" : "#999"}
+              />
+            );
           },
         })}
-        tabBarOptions={{
-          activeTintColor: "#000",
-          inactiveTintColor: "#f00",
-          activeBackgroundColor: "#999",
-          inactiveBackgroundColor: "#fff",
-          showLabel: true,
-          labelStyle: { fontSize: 14 },
-          showIcon: true,
-        }}
-        activeColor="#00f"
-        inactiveColor="#fff"
-        barStyle={{ backgroundColor: "#ff9999" }}
       >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="About" component={AboutScreen} />
@@ -58,4 +47,3 @@ function App() {
     </NavigationContainer>
   );
 }
-export default App;
